60c259d2b8f1646a3d17dba72c1f78c40c842b15
Darklit <0lies18pies@gmail.com>
2017-10-11 15:33:41 -0400

    Rework clear command (things to be added later on)

--- commands/chattools/clear.js
+++ commands/chattools/clear.js
@@ -18,7 +18,7 @@ class ClearCommand extends Commando.Command {
           key: 'amount',
           prompt: 'How many messages would you like to delete?',
           type: 'integer',
-          max: 100
+          max: 99
         },
         {
           key: 'user',
@@ -35,6 +35,37 @@ class ClearCommand extends Commando.Command {
   run(message,args){
     var messageDel = 0;
     var members = message.channel.members.array();
+    var delMessages = [];
+    if(args.user == ''){
+      message.channel.bulkDelete(args.amount+1).then(msgs => {
+        message.reply(msgs.array().length-1 + " messages deleted.");
+      })
+      .catch(err => {
+        message.reply("Error deleting messages!");
+        message.reply(err.message);
+      });
+    }else{
+      message.channel.fetchMessages({limit:100}).then(msgs => {
+        var mess = msgs.array();
+        for(var i = 0; i < mess.length; i++){
+          if(mess[i].member.displayName.toLowerCase() == args.user){
+            delMessages[delMessages.length] = mess[i];
+          }
+          if(delMessages.length >= args.amount){
+            break;
+          }
+        }
+        message.channel.bulkDelete(delMessages).then(mes => {
+          message.reply(`${mes.array().length} messages deleted.`);
+        })
+        .catch(err =>{
+          message.reply("Error deleting messages!");
+          message.reply(err.message);
+        });
+      }).catch(console.error);
+    }
+  }
+  deleteMethod2(){
 
   }
 }
