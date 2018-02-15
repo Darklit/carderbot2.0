a01cd6fd55e9004306887de417ac8302b1fc969f
Darklit <0lies18pies@gmail.com>
2018-02-15 11:24:59 -0500

    Start on new features

--- commands/music/skip.js
+++ commands/music/skip.js
@@ -20,13 +20,40 @@ class SkipCommand extends Commando.Command {
       ]
       */
     });
+    this.skipping = false;
   }
 
   run(message,args){
-    var data = this.client.registry.resolveCommand('music:play').servers[message.guild.id];
-    if(data !== undefined){
-      data.dispatcher.stop();
-      message.reply("Skipped!");
+    if(!this.skipping){
+      message.channel.send("Say I if you would like to skip the current song!").then(msg => {
+        var memberCount = message.member.voiceChannel.members.array().length-1;
+        var currentVotes = 1;
+        var voted = [message.author.id];
+        const filter = m => m.content.toLowerCase() == 'i';
+        const collector = msg.channel.createMessageCollector(filter, {time: 10000});
+        collector.on('collect', m => {
+          var tries = 0;
+          for(var i = 0; i < voted.length; i++){
+            if(voted[i] != m.author.id) tries++;
+            else break;
+          }
+          if(tries == voted.length){
+            currentVotes++;
+            voted[voted.length] = m.author.id;
+          }
+          message.channel.send(`${currentVotes} out of ${memberCount} voted`);
+          if(currentVotes>=memberCount/2){
+            collector.stop();
+            this.client.registry.resolveCommand('music:play').servers[message.guild.id].dispatcher.end();
+            message.channel.send("Skipped!");
+          }
+        });
+        if(currentVotes>=memberCount/2){
+          collector.stop();
+          this.client.registry.resolveCommand('music:play').servers[message.guild.id].dispatcher.end();
+          message.channel.send("Skipped!");
+        }
+      }).catch(console.error);
     }
   }
 }
